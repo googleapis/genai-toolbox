7c5b425dcbcf0a7d4912c354380f974e3635897c
Twisha Bansal <twishabansal@google.com>
2024-11-13 15:24:02 +0530

    Try running empty test

--- sdks/llamaindex/tests/llamaindex_sdk_integration_test.py
+++ sdks/llamaindex/tests/llamaindex_sdk_integration_test.py
@@ -1,11 +1,3 @@
-from aiohttp import ClientSession
-
-from toolbox_llamaindex_sdk import ToolboxClient
-
-
 def test_hello_world():
-    print("Creating new Toolbox client.")
-    session = ClientSession()
-    client = ToolboxClient(url="test_url", session=session)
-    print("Created toolbox client:", client)
+    print("Running empty tests.")
     assert 0 == 0
