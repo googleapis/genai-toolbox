db0e353ab4451b6dd9946117bc7c8adde0a4d330
Yuan Teoh <yuanteoh@google.com>
2024-12-05 21:37:13 -0800

    ci(cloudsql-pg): add end to end integration test

--- tests/cloud_sql_pg_integration_test.go
+++ tests/cloud_sql_pg_integration_test.go
@@ -1,7 +1,95 @@
+// Copyright 2024 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package tests
 
-import "testing"
+import (
+	"context"
+	"os"
+	"regexp"
+	"testing"
+	"time"
+)
 
-func TestHelloWorld(t *testing.T) {
-	t.Log("Hello World Test - Placeholder for Cloud SQL integration tests.")
+var (
+	CLOUD_SQL_POSTGRES_PROJECT  = os.Getenv("CLOUD_SQL_POSTGRES_PROJECT")
+	CLOUD_SQL_POSTGRES_REGION   = os.Getenv("CLOUD_SQL_POSTGRES_REGION")
+	CLOUD_SQL_POSTGRES_INSTANCE = os.Getenv("CLOUD_SQL_POSTGRES_INSTANCE")
+	CLOUD_SQL_POSTGRES_DATABASE = os.Getenv("CLOUD_SQL_POSTGRES_DATABASE")
+	CLOUD_SQL_POSTGRES_USER     = os.Getenv("CLOUD_SQL_POSTGRES_USER")
+	CLOUD_SQL_POSTGRES_PASS     = os.Getenv("CLOUD_SQL_POSTGRES_PASS")
+)
+
+func requireCloudSQLPgVars(t *testing.T) {
+	switch "" {
+	case CLOUD_SQL_POSTGRES_PROJECT:
+		t.Fatal("'CLOUD_SQL_POSTGRES_PROJECT' not set")
+	case CLOUD_SQL_POSTGRES_REGION:
+		t.Fatal("'CLOUD_SQL_POSTGRES_REGION' not set")
+	case CLOUD_SQL_POSTGRES_INSTANCE:
+		t.Fatal("'CLOUD_SQL_POSTGRES_INSTANCE' not set")
+	case CLOUD_SQL_POSTGRES_DATABASE:
+		t.Fatal("'CLOUD_SQL_POSTGRES_DATABASE' not set")
+	case CLOUD_SQL_POSTGRES_USER:
+		t.Fatal("'CLOUD_SQL_POSTGRES_USER' not set")
+	case CLOUD_SQL_POSTGRES_PASS:
+		t.Fatal("'CLOUD_SQL_POSTGRES_PASS' not set")
+	}
+}
+
+func TestCloudSQLPostgres(t *testing.T) {
+	requireCloudSQLPgVars(t)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	var args []string
+
+	// Write config into a file and pass it to command
+	toolsFile := map[string]any{
+		"sources": map[string]any{
+			"my-pg-instance": map[string]any{
+				"kind":     "cloud-sql-postgres",
+				"project":  CLOUD_SQL_POSTGRES_PROJECT,
+				"instance": CLOUD_SQL_POSTGRES_INSTANCE,
+				"region":   CLOUD_SQL_POSTGRES_REGION,
+				"database": CLOUD_SQL_POSTGRES_DATABASE,
+				"user":     CLOUD_SQL_POSTGRES_USER,
+				"password": CLOUD_SQL_POSTGRES_PASS,
+			},
+		},
+		"tools": map[string]any{
+			"my-simple-tool": map[string]any{
+				"kind":        "postgres-sql",
+				"source":      "my-pg-instance",
+				"description": "Simple tool to test end to end functionality.",
+				"statement":   "SELECT 1;",
+			},
+		},
+	}
+	cmd, cleanup, err := StartCmd(ctx, toolsFile, args...)
+	if err != nil {
+		t.Fatalf("command initialization returned an error: %s", err)
+	}
+	defer cleanup()
+
+	waitCtx, cancel := context.WithTimeout(ctx, 10*time.Minute)
+	defer cancel()
+	out, err := cmd.WaitForString(waitCtx, regexp.MustCompile(`INFO "Initialized 1 tools."`))
+	if err != nil {
+		t.Logf("toolbox command logs: \n%s", out)
+		t.Fatalf("toolbox didn't start successfully: %s", err)
+	}
+
+    // do a request
 }
