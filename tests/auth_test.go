26579a81c94bc012e0b7db214b92eba70f2db43b
duwenxin <duwenxin@google.com>
2024-12-30 18:00:05 -0500

    ci(auth): Increase OAuth integration test coverage

--- tests/auth_test.go
+++ tests/auth_test.go
@@ -23,14 +23,17 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"os"
 	"os/exec"
 	"regexp"
+	"strings"
 	"time"
 
 	"testing"
 
 	"github.com/googleapis/genai-toolbox/internal/auth"
 	"github.com/googleapis/genai-toolbox/internal/auth/google"
+	"github.com/googleapis/genai-toolbox/internal/sources/cloudsqlpg"
 )
 
 var clientId string = "32555940559.apps.googleusercontent.com"
@@ -42,8 +45,27 @@ var (
 	CLOUD_SQL_POSTGRES_DATABASE = os.Getenv("CLOUD_SQL_POSTGRES_DATABASE")
 	CLOUD_SQL_POSTGRES_USER     = os.Getenv("CLOUD_SQL_POSTGRES_USER")
 	CLOUD_SQL_POSTGRES_PASS     = os.Getenv("CLOUD_SQL_POSTGRES_PASS")
-	SERVICE_ACCOUNT_EMAIL 		= os.Getenv("SERVICE_ACCOUNT_EMAIL")
+	SERVICE_ACCOUNT_EMAIL       = os.Getenv("SERVICE_ACCOUNT_EMAIL")
 )
+
+func requireCloudSQLPgVars(t *testing.T) {
+	switch "" {
+	case CLOUD_SQL_POSTGRES_PROJECT:
+		t.Fatal("'CLOUD_SQL_POSTGRES_PROJECT' not set")
+	case CLOUD_SQL_POSTGRES_REGION:
+		t.Fatal("'CLOUD_SQL_POSTGRES_REGION' not set")
+	case CLOUD_SQL_POSTGRES_INSTANCE:
+		t.Fatal("'CLOUD_SQL_POSTGRES_INSTANCE' not set")
+	case CLOUD_SQL_POSTGRES_DATABASE:
+		t.Fatal("'CLOUD_SQL_POSTGRES_DATABASE' not set")
+	case CLOUD_SQL_POSTGRES_USER:
+		t.Fatal("'CLOUD_SQL_POSTGRES_USER' not set")
+	case CLOUD_SQL_POSTGRES_PASS:
+		t.Fatal("'CLOUD_SQL_POSTGRES_PASS' not set")
+	case SERVICE_ACCOUNT_EMAIL:
+		t.Fatal("'CLOUD_SQL_POSTGRES_PASS' not set")
+	}
+}
 
 // Get a Google ID token
 func getGoogleIdToken(audience string) (string, error) {
@@ -51,7 +73,7 @@ func getGoogleIdToken(audience string) (string, error) {
 	cmd := exec.Command("gcloud", "auth", "print-identity-token")
 	output, err := cmd.Output()
 	if err == nil {
-		return string(output), nil
+		return strings.TrimSpace(string(output)), nil
 	} else {
 		// Cloud Build testing
 		url := "http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/identity?audience=" + audience
@@ -75,7 +97,6 @@ func getGoogleIdToken(audience string) (string, error) {
 }
 
 func TestGoogleAuthVerification(t *testing.T) {
-
 	tcs := []struct {
 		authSource auth.AuthSource
 		isErr      bool
@@ -116,6 +137,7 @@ func TestGoogleAuthVerification(t *testing.T) {
 			}
 		}
 
+		// Check if decoded claims are valid
 		_, ok := claims["sub"]
 		if !ok {
 			if tc.isErr {
@@ -128,43 +150,37 @@ func TestGoogleAuthVerification(t *testing.T) {
 }
 
 func TestAuthenticatedParameter(t *testing.T) {
-	requireCloudSQLPgVars(t)
-	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
-	defer cancel()
-
-	var args []string
-
 	// Set up test table
-	pool, err := initCloudSQLPgConnectionPool(CLOUD_SQL_POSTGRES_PROJECT, CLOUD_SQL_POSTGRES_REGION, CLOUD_SQL_POSTGRES_INSTANCE, "public", CLOUD_SQL_POSTGRES_USER, CLOUD_SQL_POSTGRES_PASS, CLOUD_SQL_POSTGRES_DATABASE)
+	pool, err := cloudsqlpg.InitCloudSQLPgConnectionPool(CLOUD_SQL_POSTGRES_PROJECT, CLOUD_SQL_POSTGRES_REGION, CLOUD_SQL_POSTGRES_INSTANCE, "public", CLOUD_SQL_POSTGRES_USER, CLOUD_SQL_POSTGRES_PASS, CLOUD_SQL_POSTGRES_DATABASE)
 	if err != nil {
-		return nil, fmt.Errorf("unable to create pool: %w", err)
+		t.Fatalf("unable to create Cloud SQL connection pool: %s", err)
 	}
 
 	err = pool.Ping(context.Background())
 	if err != nil {
-		return nil, fmt.Errorf("unable to connect successfully: %w", err)
+		t.Fatalf("unable to connect to test database: %s", err)
 	}
-	
-	results, err := pool.Query(context.Background(), `
+
+	_, err = pool.Query(context.Background(), `
 		CREATE TABLE auth_table (
-			id INT AUTO_INCREMENT PRIMARY KEY,
+			id SERIAL PRIMARY KEY,
 			name TEXT,
 			email TEXT
 		);
 	`)
-	stmt, err := pool.Prepare(context.Background(), `
+	if err != nil {
+		t.Fatalf("unable to create test table: %s", err)
+	}
+
+	// Insert test data
+	statement := `
 		INSERT INTO auth_table (name, email) 
 		VALUES ($1, $2), ($3, $4)
-	`)
+	`
+	params := []any{"Alice", SERVICE_ACCOUNT_EMAIL, "Jane", "janedoe@gmail.com"}
+	_, err = pool.Query(context.Background(), statement, params...)
 	if err != nil {
-		return nil, fmt.Errorf("unable to prepare test statement: %w", err)
-	}
-	results, err := stmt.Exec(context.Background(),
-		"Alice", SERVICE_ACCOUNT_EMAIL, 
-		"Jane", "janedoe@gmail.com",
-	)
-	if err != nil {
-		return nil, fmt.Errorf("unable to insert rows into test database: %w", err)
+		t.Fatalf("unable to insert test data: %s", err)
 	}
 	defer pool.Exec(context.Background(), `DROP TABLE auth_table;`)
 
@@ -182,7 +198,7 @@ func TestAuthenticatedParameter(t *testing.T) {
 			},
 		},
 		"authSources": map[string]any{
-			"my-google-auth-service": map[string]any{
+			"my-google-auth": map[string]any{
 				"kind":     "google",
 				"clientId": clientId,
 			},
@@ -192,15 +208,15 @@ func TestAuthenticatedParameter(t *testing.T) {
 				"kind":        "postgres-sql",
 				"source":      "my-pg-instance",
 				"description": "Tool to test authenticated parameters.",
-				"statement":   "SELECT * WHERE email = $1;",
+				"statement":   "SELECT * FROM auth_table WHERE email = $1;",
 				"parameters": []map[string]any{
 					{
-						"name":        "my_string",
+						"name":        "email",
 						"type":        "string",
-						"description": "this param is a string",
+						"description": "user email",
 						"authSources": []map[string]string{
 							{
-								"name":  "my-google-auth-service",
+								"name":  "my-google-auth",
 								"field": "email",
 							},
 						},
@@ -209,6 +225,12 @@ func TestAuthenticatedParameter(t *testing.T) {
 			},
 		},
 	}
+	// Initialize a test command
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	var args []string
+
 	cmd, cleanup, err := StartCmd(ctx, toolsFile, args...)
 	if err != nil {
 		t.Fatalf("command initialization returned an error: %s", err)
@@ -223,40 +245,72 @@ func TestAuthenticatedParameter(t *testing.T) {
 		t.Fatalf("toolbox didn't start successfully: %s", err)
 	}
 
+	// Get ID token
+	idToken, err := getGoogleIdToken(clientId)
+	if err != nil {
+		t.Fatalf("error getting Google ID token: %s", err)
+	}
+
+	// Create wanted string
+	wantResult := fmt.Sprintf("Stub tool call for \"my-auth-tool\"! Parameters parsed: [{\"email\" \"%s\"}] \n Output: [%%!s(int32=1) Alice %s]", SERVICE_ACCOUNT_EMAIL, SERVICE_ACCOUNT_EMAIL)
+
 	// Test tool invocation with authenticated parameters
 	invokeTcs := []struct {
-		name        string
-		api         string
-		requestBody io.Reader
-		want        string
+		name          string
+		api           string
+		requestHeader map[string]string
+		requestBody   io.Reader
+		want          string
+		isErr         bool
 	}{
 		{
-			name:        "invoke my-auth-tool",
-			api:         "http://127.0.0.1:5000/api/tool/my-auth-tool/invoke",
-			requestHeader: map[string]string{"my-google-auth_token":getGoogleIdToken(clientId)}
-			requestBody: bytes.NewBuffer([]byte(`{}`)),
-			want:        "Stub tool call for \"my-simple-tool\"! Parameters parsed: [] \n Output: [%!s(int32=1)]",
+			name:          "Invoke my-auth-tool with auth token",
+			api:           "http://127.0.0.1:5000/api/tool/my-auth-tool/invoke",
+			requestHeader: map[string]string{"my-google-auth_token": idToken},
+			requestBody:   bytes.NewBuffer([]byte(`{}`)),
+			isErr:         false,
+			want:          wantResult,
+		},
+		{
+			name:          "Invoke my-auth-tool with invalid auth token",
+			api:           "http://127.0.0.1:5000/api/tool/my-auth-tool/invoke",
+			requestHeader: map[string]string{"my-google-auth_token": "INVALID_TOKEN"},
+			requestBody:   bytes.NewBuffer([]byte(`{}`)),
+			isErr:         true,
+		},
+		{
+			name:          "Invoke my-auth-tool without auth token",
+			api:           "http://127.0.0.1:5000/api/tool/my-auth-tool/invoke",
+			requestHeader: map[string]string{},
+			requestBody:   bytes.NewBuffer([]byte(`{}`)),
+			isErr:         true,
 		},
 	}
 	for _, tc := range invokeTcs {
 		t.Run(tc.name, func(t *testing.T) {
-			req, err := http.NewRequest(method, tc.api, tc.requestBody)
+			// Send Tool invocation request with ID token
+			req, err := http.NewRequest(http.MethodPost, tc.api, tc.requestBody)
 			if err != nil {
-				return nil, nil, fmt.Errorf("unable to create request: %w", err)
+				t.Fatalf("unable to create request: %s", err)
 			}
 			req.Header.Add("Content-type", "application/json")
-			for k, v := range tc.requestHeader{
+			for k, v := range tc.requestHeader {
 				req.Header.Add(k, v)
 			}
 			resp, err := http.DefaultClient.Do(req)
 			if err != nil {
-				return nil, nil, fmt.Errorf("unable to send request: %w", err)
+				t.Fatalf("unable to send request: %s", err)
 			}
 
-			if resp.StatusCode != 200 {
-				t.Fatalf("response status code is not 200")
+			if resp.StatusCode != http.StatusOK {
+				if tc.isErr == true {
+					return
+				}
+				bodyBytes, _ := io.ReadAll(resp.Body)
+				t.Fatalf("response status code is not 200, got %d: %s", resp.StatusCode, string(bodyBytes))
 			}
 
+			// Check response body
 			var body map[string]interface{}
 			err = json.NewDecoder(resp.Body).Decode(&body)
 			if err != nil {
@@ -272,6 +326,138 @@ func TestAuthenticatedParameter(t *testing.T) {
 			}
 		})
 	}
-	
+}
 
+func TestAuthRequiredToolInvocation(t *testing.T) {
+	// Write config into a file and pass it to command
+	toolsFile := map[string]any{
+		"sources": map[string]any{
+			"my-pg-instance": map[string]any{
+				"kind":     "cloud-sql-postgres",
+				"project":  CLOUD_SQL_POSTGRES_PROJECT,
+				"instance": CLOUD_SQL_POSTGRES_INSTANCE,
+				"region":   CLOUD_SQL_POSTGRES_REGION,
+				"database": CLOUD_SQL_POSTGRES_DATABASE,
+				"user":     CLOUD_SQL_POSTGRES_USER,
+				"password": CLOUD_SQL_POSTGRES_PASS,
+			},
+		},
+		"authSources": map[string]any{
+			"my-google-auth": map[string]any{
+				"kind":     "google",
+				"clientId": clientId,
+			},
+		},
+		"tools": map[string]any{
+			"my-auth-tool": map[string]any{
+				"kind":        "postgres-sql",
+				"source":      "my-pg-instance",
+				"description": "Tool to test authenticated parameters.",
+				"statement":   "SELECT 1;",
+				"authRequired": []string{
+					"my-google-auth",
+				},
+			},
+		},
+	}
+
+	// Initialize a test command
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	var args []string
+
+	cmd, cleanup, err := StartCmd(ctx, toolsFile, args...)
+	if err != nil {
+		t.Fatalf("command initialization returned an error: %s", err)
+	}
+	defer cleanup()
+
+	waitCtx, cancel := context.WithTimeout(ctx, 10*time.Minute)
+	defer cancel()
+
+	out, err := cmd.WaitForString(waitCtx, regexp.MustCompile(`INFO "Server ready to serve"`))
+	if err != nil {
+		t.Logf("toolbox command logs: \n%s", out)
+		t.Fatalf("toolbox didn't start successfully: %s", err)
+	}
+
+	// Get ID token
+	idToken, err := getGoogleIdToken(clientId)
+	if err != nil {
+		t.Fatalf("error getting Google ID token: %s", err)
+	}
+
+	// Test auth-required Tool invocation
+	invokeTcs := []struct {
+		name          string
+		api           string
+		requestHeader map[string]string
+		requestBody   io.Reader
+		want          string
+		isErr         bool
+	}{
+		{
+			name:          "Invoke my-auth-tool with auth token",
+			api:           "http://127.0.0.1:5000/api/tool/my-auth-tool/invoke",
+			requestHeader: map[string]string{"my-google-auth_token": idToken},
+			requestBody:   bytes.NewBuffer([]byte(`{}`)),
+			isErr:         false,
+			want:          "Stub tool call for \"my-auth-tool\"! Parameters parsed: [] \n Output: [%!s(int32=1)]",
+		},
+		{
+			name:          "Invoke my-auth-tool with invalid auth token",
+			api:           "http://127.0.0.1:5000/api/tool/my-auth-tool/invoke",
+			requestHeader: map[string]string{"my-google-auth_token": "INVALID_TOKEN"},
+			requestBody:   bytes.NewBuffer([]byte(`{}`)),
+			isErr:         true,
+		},
+		{
+			name:          "Invoke my-auth-tool without auth token",
+			api:           "http://127.0.0.1:5000/api/tool/my-auth-tool/invoke",
+			requestHeader: map[string]string{},
+			requestBody:   bytes.NewBuffer([]byte(`{}`)),
+			isErr:         true,
+		},
+	}
+	for _, tc := range invokeTcs {
+		t.Run(tc.name, func(t *testing.T) {
+			// Send Tool invocation request with ID token
+			req, err := http.NewRequest(http.MethodPost, tc.api, tc.requestBody)
+			if err != nil {
+				t.Fatalf("unable to create request: %s", err)
+			}
+			req.Header.Add("Content-type", "application/json")
+			for k, v := range tc.requestHeader {
+				req.Header.Add(k, v)
+			}
+			resp, err := http.DefaultClient.Do(req)
+			if err != nil {
+				t.Fatalf("unable to send request: %s", err)
+			}
+
+			if resp.StatusCode != http.StatusOK {
+				if tc.isErr == true {
+					return
+				}
+				bodyBytes, _ := io.ReadAll(resp.Body)
+				t.Fatalf("response status code is not 200, got %d: %s", resp.StatusCode, string(bodyBytes))
+			}
+
+			// Check response body
+			var body map[string]interface{}
+			err = json.NewDecoder(resp.Body).Decode(&body)
+			if err != nil {
+				t.Fatalf("error parsing response body")
+			}
+			got, ok := body["result"].(string)
+			if !ok {
+				t.Fatalf("unable to find result in response body")
+			}
+
+			if got != tc.want {
+				t.Fatalf("unexpected value: got %q, want %q", got, tc.want)
+			}
+		})
+	}
 }
