e85f78fd33c05845711da47ccc733776c1829474
Anubhav Dhawan <anubhavdhawan@google.com>
2025-02-04 20:55:20 +0530

    fix: Fix ToolboxToolClient unit tests.

--- sdks/langchain/tests/test_client.py
+++ sdks/langchain/tests/test_client.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import asyncio
 from unittest.mock import Mock, patch
+from pydantic import BaseModel
 
 import pytest
 
@@ -39,53 +39,73 @@ class TestToolboxClient:
     @patch("toolbox_langchain_sdk.client.AsyncToolboxClient.aload_tool")
     def test_load_tool(self, mock_aload_tool, toolbox_client):
         mock_tool = Mock(spec=ToolboxTool)
-        mock_aload_tool.return_value = asyncio.Future()
-        mock_aload_tool.return_value.set_result(mock_tool)
+        mock_tool.name="mock-tool"
+        mock_tool.description="mock description"
+        mock_tool.args_schema = BaseModel
+        mock_aload_tool.return_value = mock_tool
 
-        tool = toolbox_client.load_tool("test_tool").result()
+        tool = toolbox_client.load_tool("test_tool")
 
-        assert tool == mock_tool
+        assert tool.name == mock_tool.name
+        assert tool.description == mock_tool.description
+        assert tool.args_schema == mock_tool.args_schema
         mock_aload_tool.assert_called_once_with("test_tool", {}, None, {}, True)
 
     @patch("toolbox_langchain_sdk.client.AsyncToolboxClient.aload_toolset")
     def test_load_toolset(self, mock_aload_toolset, toolbox_client):
         mock_tools = [Mock(spec=ToolboxTool), Mock(spec=ToolboxTool)]
-        mock_aload_toolset.return_value = asyncio.Future()
-        mock_aload_toolset.return_value.set_result(mock_tools)
+        mock_tools[0].name="mock-tool-0"
+        mock_tools[0].description="mock description 0"
+        mock_tools[0].args_schema = BaseModel
+        mock_tools[1].name="mock-tool-1"
+        mock_tools[1].description="mock description 1"
+        mock_tools[1].args_schema = BaseModel
+        mock_aload_toolset.return_value = mock_tools
 
-        tools = toolbox_client.load_toolset().result()
-
-        assert tools == mock_tools
+        tools = toolbox_client.load_toolset()
+        assert len(tools) == len(mock_tools)
+        assert all(a.name == b.name and a.description == b.description and a.args_schema == b.args_schema for a,b in zip(tools, mock_tools))
         mock_aload_toolset.assert_called_once_with(None, {}, None, {}, True)
 
     @pytest.mark.asyncio
     @patch("toolbox_langchain_sdk.client.AsyncToolboxClient.aload_tool")
     async def test_aload_tool(self, mock_aload_tool, toolbox_client):
         mock_tool = Mock(spec=ToolboxTool)
-
-        mock_aload_tool.return_value = asyncio.Future()
-        mock_aload_tool.return_value.set_result(mock_tool)
+        mock_tool.name="mock-tool"
+        mock_tool.description="mock description"
+        mock_tool.args_schema = BaseModel
+        mock_aload_tool.return_value = mock_tool
 
         tool = await toolbox_client.aload_tool("test_tool")
-        assert tool.result() == mock_tool
+        assert tool.name == mock_tool.name
+        assert tool.description == mock_tool.description
+        assert tool.args_schema == mock_tool.args_schema
         mock_aload_tool.assert_called_once_with("test_tool", {}, None, {}, True)
 
     @pytest.mark.asyncio
     @patch("toolbox_langchain_sdk.client.AsyncToolboxClient.aload_toolset")
     async def test_aload_toolset(self, mock_aload_toolset, toolbox_client):
         mock_tools = [Mock(spec=ToolboxTool), Mock(spec=ToolboxTool)]
-        mock_aload_toolset.return_value = asyncio.Future()
-        mock_aload_toolset.return_value.set_result(mock_tools)
+        mock_tools[0].name="mock-tool-0"
+        mock_tools[0].description="mock description 0"
+        mock_tools[0].args_schema = BaseModel
+        mock_tools[1].name="mock-tool-1"
+        mock_tools[1].description="mock description 1"
+        mock_tools[1].args_schema = BaseModel
+        mock_aload_toolset.return_value = mock_tools
 
         tools = await toolbox_client.aload_toolset()
-        assert tools.result() == mock_tools
+        assert len(tools) == len(mock_tools)
+        assert all(a.name == b.name and a.description == b.description and a.args_schema == b.args_schema for a,b in zip(tools, mock_tools))
         mock_aload_toolset.assert_called_once_with(None, {}, None, {}, True)
 
     @patch("toolbox_langchain_sdk.client.AsyncToolboxClient.aload_tool")
     def test_load_tool_with_args(self, mock_aload_tool, toolbox_client):
         mock_tool = Mock(spec=ToolboxTool)
-        mock_aload_tool.return_value = asyncio.Future()
-        mock_aload_tool.return_value.set_result(mock_tool)
+        mock_tool.name="mock-tool"
+        mock_tool.description="mock description"
+        mock_tool.args_schema = BaseModel
+        mock_aload_tool.return_value = mock_tool
         auth_tokens = {"token1": lambda: "value1"}
         auth_headers = {"header1": lambda: "value2"}
         bound_params = {"param1": "value3"}
@@ -96,9 +116,11 @@ class TestToolboxClient:
             auth_headers=auth_headers,
             bound_params=bound_params,
             strict=False,
-        ).result()
+        )
 
-        assert tool == mock_tool
+        assert tool.name == mock_tool.name
+        assert tool.description == mock_tool.description
+        assert tool.args_schema == mock_tool.args_schema
         mock_aload_tool.assert_called_once_with(
             "test_tool_name", auth_tokens, auth_headers, bound_params, False
         )
@@ -106,8 +128,13 @@ class TestToolboxClient:
     @patch("toolbox_langchain_sdk.client.AsyncToolboxClient.aload_toolset")
     def test_load_toolset_with_args(self, mock_aload_toolset, toolbox_client):
         mock_tools = [Mock(spec=ToolboxTool), Mock(spec=ToolboxTool)]
-        mock_aload_toolset.return_value = asyncio.Future()
-        mock_aload_toolset.return_value.set_result(mock_tools)
+        mock_tools[0].name="mock-tool-0"
+        mock_tools[0].description="mock description 0"
+        mock_tools[0].args_schema = BaseModel
+        mock_tools[1].name="mock-tool-1"
+        mock_tools[1].description="mock description 1"
+        mock_tools[1].args_schema = BaseModel
+        mock_aload_toolset.return_value = mock_tools
 
         auth_tokens = {"token1": lambda: "value1"}
         auth_headers = {"header1": lambda: "value2"}
@@ -119,9 +146,10 @@ class TestToolboxClient:
             auth_headers=auth_headers,
             bound_params=bound_params,
             strict=False,
-        ).result()
+        )
 
-        assert tools == mock_tools
+        assert len(tools) == len(mock_tools)
+        assert all(a.name == b.name and a.description == b.description and a.args_schema == b.args_schema for a,b in zip(tools, mock_tools))
         mock_aload_toolset.assert_called_once_with(
             "my_toolset", auth_tokens, auth_headers, bound_params, False
         )
@@ -130,8 +158,10 @@ class TestToolboxClient:
     @patch("toolbox_langchain_sdk.client.AsyncToolboxClient.aload_tool")
     async def test_aload_tool_with_args(self, mock_aload_tool, toolbox_client):
         mock_tool = Mock(spec=ToolboxTool)
-        mock_aload_tool.return_value = asyncio.Future()
-        mock_aload_tool.return_value.set_result(mock_tool)
+        mock_tool.name="mock-tool"
+        mock_tool.description="mock description"
+        mock_tool.args_schema = BaseModel
+        mock_aload_tool.return_value = mock_tool
 
         auth_tokens = {"token1": lambda: "value1"}
         auth_headers = {"header1": lambda: "value2"}
@@ -140,7 +170,9 @@ class TestToolboxClient:
         tool = await toolbox_client.aload_tool(
             "test_tool", auth_tokens, auth_headers, bound_params, False
         )
-        assert tool.result() == mock_tool
+        assert tool.name == mock_tool.name
+        assert tool.description == mock_tool.description
+        assert tool.args_schema == mock_tool.args_schema
         mock_aload_tool.assert_called_once_with(
             "test_tool", auth_tokens, auth_headers, bound_params, False
         )
@@ -149,8 +181,13 @@ class TestToolboxClient:
     @patch("toolbox_langchain_sdk.client.AsyncToolboxClient.aload_toolset")
     async def test_aload_toolset_with_args(self, mock_aload_toolset, toolbox_client):
         mock_tools = [Mock(spec=ToolboxTool), Mock(spec=ToolboxTool)]
-        mock_aload_toolset.return_value = asyncio.Future()
-        mock_aload_toolset.return_value.set_result(mock_tools)
+        mock_tools[0].name="mock-tool-0"
+        mock_tools[0].description="mock description 0"
+        mock_tools[0].args_schema = BaseModel
+        mock_tools[1].name="mock-tool-1"
+        mock_tools[1].description="mock description 1"
+        mock_tools[1].args_schema = BaseModel
+        mock_aload_toolset.return_value = mock_tools
 
         auth_tokens = {"token1": lambda: "value1"}
         auth_headers = {"header1": lambda: "value2"}
@@ -159,7 +196,8 @@ class TestToolboxClient:
         tools = await toolbox_client.aload_toolset(
             "my_toolset", auth_tokens, auth_headers, bound_params, False
         )
-        assert tools.result() == mock_tools
+        assert len(tools) == len(mock_tools)
+        assert all(a.name == b.name and a.description == b.description and a.args_schema == b.args_schema for a,b in zip(tools, mock_tools))
         mock_aload_toolset.assert_called_once_with(
             "my_toolset", auth_tokens, auth_headers, bound_params, False
         )
