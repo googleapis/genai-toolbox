8354a145504877779b31344af8c26ecf4e934a1b
Yuan Teoh <yuanteoh@google.com>
2025-04-01 11:14:26 -0700

    feat: support sse

--- internal/server/mcp/types.go
+++ internal/server/mcp/types.go
@@ -291,5 +291,6 @@ type CallToolResult struct {
 	Content []TextContent `json:"content"`
 	// Whether the tool call ended in an error.
 	// If not set, this is assumed to be false (the call was successful).
-	IsError bool `json:"isError,omitempty"`
+	IsError bool                `json:"isError,omitempty"`
+	Tools   []tools.McpManifest `json:"tools"`
 }
