c8fcc7d889e74db7e6e1e706572807f67bcf8be8
Yuan Teoh <yuanteoh@google.com>
2025-04-23 14:31:57 -0700

    separate tests into own pacakges

--- tests/neo4j/neo4j_integration_test.go
+++ tests/neo4j/neo4j_integration_test.go
@@ -1,6 +1,4 @@
-//go:build integration
-
-// Copyright 2025 Google LLC
+// Copyright 2024 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package tests
+package neo4j
 
 import (
 	"bytes"
@@ -27,28 +25,42 @@ import (
 	"regexp"
 	"testing"
 	"time"
+
+	"github.com/googleapis/genai-toolbox/tests"
 )
 
 var (
-	DGRAPH_SOURCE_KIND = "dgraph"
-	DGRAPH_TOOL_KIND   = "dgraph-dql"
-	DGRAPH_API_KEY     = "api-key"
-	DGRAPH_URL         = os.Getenv("DGRAPH_URL")
+	NEO4J_SOURCE_KIND = "neo4j"
+	NEO4J_TOOL_KIND   = "neo4j-cypher"
+	NEO4J_DATABASE    = os.Getenv("NEO4J_DATABASE")
+	NEO4J_URI         = os.Getenv("NEO4J_URI")
+	NEO4J_USER        = os.Getenv("NEO4J_USER")
+	NEO4J_PASS        = os.Getenv("NEO4J_PASS")
 )
 
-func getDgraphVars(t *testing.T) map[string]any {
-	if DGRAPH_URL == "" {
-		t.Fatal("'DGRAPH_URL' not set")
+func getNeo4jVars(t *testing.T) map[string]any {
+	switch "" {
+	case NEO4J_DATABASE:
+		t.Fatal("'NEO4J_DATABASE' not set")
+	case NEO4J_URI:
+		t.Fatal("'NEO4J_URI' not set")
+	case NEO4J_USER:
+		t.Fatal("'NEO4J_USER' not set")
+	case NEO4J_PASS:
+		t.Fatal("'NEO4J_PASS' not set")
 	}
+
 	return map[string]any{
-		"kind":      DGRAPH_SOURCE_KIND,
-		"dgraphUrl": DGRAPH_URL,
-		"apiKey":    DGRAPH_API_KEY,
+		"kind":     NEO4J_SOURCE_KIND,
+		"uri":      NEO4J_URI,
+		"database": NEO4J_DATABASE,
+		"user":     NEO4J_USER,
+		"password": NEO4J_PASS,
 	}
 }
 
-func TestDgraphToolEndpoints(t *testing.T) {
-	sourceConfig := getDgraphVars(t)
+func TestNeo4jToolEndpoints(t *testing.T) {
+	sourceConfig := getNeo4jVars(t)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
@@ -57,21 +69,18 @@ func TestDgraphToolEndpoints(t *testing.T) {
 	// Write config into a file and pass it to command
 	toolsFile := map[string]any{
 		"sources": map[string]any{
-			"my-dgraph-instance": sourceConfig,
+			"my-neo4j-instance": sourceConfig,
 		},
 		"tools": map[string]any{
-			"my-simple-dql-tool": map[string]any{
-				"kind":        "dgraph-dql",
-				"source":      "my-dgraph-instance",
+			"my-simple-cypher-tool": map[string]any{
+				"kind":        "neo4j-cypher",
+				"source":      "my-neo4j-instance",
 				"description": "Simple tool to test end to end functionality.",
-				"statement":   "{result(func: uid(0x0)) {constant: math(1)}}",
-				"isQuery":     true,
-				"timeout":     "20s",
-				"parameters":  []any{},
+				"statement":   "RETURN 1 as a;",
 			},
 		},
 	}
-	cmd, cleanup, err := StartCmd(ctx, toolsFile, args...)
+	cmd, cleanup, err := tests.StartCmd(ctx, toolsFile, args...)
 	if err != nil {
 		t.Fatalf("command initialization returned an error: %s", err)
 	}
@@ -93,9 +102,9 @@ func TestDgraphToolEndpoints(t *testing.T) {
 	}{
 		{
 			name: "get my-simple-tool",
-			api:  "http://127.0.0.1:5000/api/tool/my-simple-dql-tool/",
+			api:  "http://127.0.0.1:5000/api/tool/my-simple-cypher-tool/",
 			want: map[string]any{
-				"my-simple-dql-tool": map[string]any{
+				"my-simple-cypher-tool": map[string]any{
 					"description":  "Simple tool to test end to end functionality.",
 					"parameters":   []any{},
 					"authRequired": []any{},
@@ -138,10 +147,10 @@ func TestDgraphToolEndpoints(t *testing.T) {
 		want        string
 	}{
 		{
-			name:        "invoke my-simple-dql-tool",
-			api:         "http://127.0.0.1:5000/api/tool/my-simple-dql-tool/invoke",
+			name:        "invoke my-simple-cypher-tool",
+			api:         "http://127.0.0.1:5000/api/tool/my-simple-cypher-tool/invoke",
 			requestBody: bytes.NewBuffer([]byte(`{}`)),
-			want:        "[{\"result\":[{\"constant\":1}]}]",
+			want:        "[{\"a\":1}]",
 		},
 	}
 	for _, tc := range invokeTcs {
@@ -162,7 +171,6 @@ func TestDgraphToolEndpoints(t *testing.T) {
 				t.Fatalf("error parsing response body")
 			}
 			got, ok := body["result"].(string)
-
 			if !ok {
 				t.Fatalf("unable to find result in response body")
 			}
