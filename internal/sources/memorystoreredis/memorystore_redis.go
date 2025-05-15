1b0236998fe4a7bb413c2f3fa904115fe68ad4b8
duwenxin <duwenxin@google.com>
2025-05-15 13:56:39 -0400

    update unit test

--- internal/sources/memorystoreredis/memorystore_redis.go
+++ internal/sources/memorystoreredis/memorystore_redis.go
@@ -37,6 +37,10 @@ type Config struct {
 	Database       int    `yaml:"database"`
 }
 
+func (r Config) SourceConfigKind() string {
+	return SourceKind
+}
+
 // RedisClient is an interface for `redis.Client` and `redis.ClusterClient
 type RedisClient interface {
 	Do(context.Context, ...any) *redis.Cmd
@@ -44,10 +48,6 @@ type RedisClient interface {
 
 var _ RedisClient = (*redis.Client)(nil)
 var _ RedisClient = (*redis.ClusterClient)(nil)
-
-func (r Config) SourceConfigKind() string {
-	return SourceKind
-}
 
 func (r Config) Initialize(ctx context.Context, tracer trace.Tracer) (sources.Source, error) {
 	client, err := initMemorystoreRedisClient(ctx, r)
