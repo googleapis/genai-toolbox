fe28a9a6cfa90d11bbf06bd48ea72c88537399a6
Prerna Kakkar <prernakakkar@google.com>
2025-05-20 17:04:11 +0000

    fix typo

--- internal/tools/mssqlexecutesql/mssqlexecutesql_test.go
+++ internal/tools/mssqlexecutesql/mssqlexecutesql_test.go
@@ -39,7 +39,7 @@ func TestParseFromYamlExecuteSql(t *testing.T) {
 			in: `
 			tools:
 				example_tool:
-					kind: cloud-sql-mssql
+					kind: mssql-execute-sql
 					source: my-instance
 					description: some description
 					authRequired:
