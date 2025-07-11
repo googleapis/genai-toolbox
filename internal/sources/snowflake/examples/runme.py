247d2ea14b0fe51bfc9850a8a971e6b86aa4fa76
igor-elbert <83839202+igor-elbert@users.noreply.github.com>
2025-07-11 14:43:56 -0400

    Changed example table name to a more common

--- internal/sources/snowflake/examples/runme.py
+++ internal/sources/snowflake/examples/runme.py
@@ -1,6 +1,7 @@
 import asyncio
 from toolbox_core import ToolboxClient
 
+
 async def main():
     # Replace with the actual URL where your Toolbox service is running
     async with ToolboxClient("http://127.0.0.1:5000") as toolbox:
@@ -9,8 +10,9 @@ async def main():
         print(result)
 
         tool = await toolbox.load_tool("list_tables")
-        result = await tool("DIM_AWS")
+        result = await tool(table_names="DIM_DATE")
         print(result)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
