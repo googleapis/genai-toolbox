99f41016c195fd67f4a89ad3bca76b3e78ed95a0
Huan Chen <huanc@google.com>
2025-07-18 00:31:34 +0000

    add comment to dry run

--- internal/tools/bigquery/bigqueryexecutesql/bigqueryexecutesql.go
+++ internal/tools/bigquery/bigqueryexecutesql/bigqueryexecutesql.go
@@ -199,6 +199,7 @@ func (t Tool) Authorized(verifiedAuthServices []string) bool {
 	return tools.IsAuthorized(t.AuthRequired, verifiedAuthServices)
 }
 
+// dryRunQuery performs a dry run of the SQL query to validate it and get metadata.
 func dryRunQuery(ctx context.Context, restService *bigqueryrestapi.Service, projectID string, sql string) (*bigqueryrestapi.Job, error) {
 	useLegacySql := false
 	jobToInsert := &bigqueryrestapi.Job{
