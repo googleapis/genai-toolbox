c1d248c17639b7a83fa64bbc0591b05eb621cdcc
Huan Chen <142538604+Genesis929@users.noreply.github.com>
2025-07-17 17:14:26 -0700

    Update tests/bigquery/bigquery_integration_test.go
    Co-authored-by: Yuan Teoh <45984206+Yuan325@users.noreply.github.com>

--- tests/bigquery/bigquery_integration_test.go
+++ tests/bigquery/bigquery_integration_test.go
@@ -380,7 +380,7 @@ func runBigQueryExecuteSqlToolInvokeTest(t *testing.T, select1Want, invokeParamW
 			api:           "http://127.0.0.1:5000/api/tool/my-exec-sql-tool/invoke",
 			requestHeader: map[string]string{},
 			requestBody:   bytes.NewBuffer([]byte(`{"sql":"CREATE TABLE t (id SERIAL PRIMARY KEY, name TEXT)"}`)),
-			want:          "\"Operation completed successfully.\"",
+			want:          `"Operation completed successfully."`,
 			isErr:         true,
 		},
 		{
