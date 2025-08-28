afe55a3c419e54e45b62396ace5e97810f7ff535
duwenxin99 <duwenxin@google.com>
2025-08-28 17:19:35 -0400

    client location

--- internal/tools/bigquery/bigquerysql/bigquerysql.go
+++ internal/tools/bigquery/bigquerysql/bigquerysql.go
@@ -232,9 +232,9 @@ func (t Tool) Invoke(ctx context.Context, params tools.ParamValues, accessToken 
 
 	query = bqClient.Query(newStatement)
 	query.Parameters = highLevelParams
-	query.Location = t.Client.Location
+	query.Location = bqClient.Location
 
-	dryRunJob, err := dryRunQuery(ctx, restService, t.Client.Project(), t.Client.Location, newStatement, lowLevelParams, query.ConnectionProperties)
+	dryRunJob, err := dryRunQuery(ctx, restService, bqClient.Project(), bqClient.Location, newStatement, lowLevelParams, query.ConnectionProperties)
 	if err != nil {
 		// This is a fallback check in case the switch logic was bypassed.
 		return nil, fmt.Errorf("final query validation failed: %w", err)
