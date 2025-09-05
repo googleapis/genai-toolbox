6688f9e7a5585d670421f2b528c41363626e7975
Huan Chen <huanc@google.com>
2025-09-05 01:07:32 +0000

    fix merge error

--- internal/tools/bigquery/bigquerylisttableids/bigquerylisttableids.go
+++ internal/tools/bigquery/bigquerylisttableids/bigquerylisttableids.go
@@ -88,7 +88,7 @@ func (cfg Config) Initialize(srcs map[string]sources.Source) (tools.Tool, error)
 		return nil, fmt.Errorf("invalid source for %q tool: source kind must be one of %q", kind, compatibleSources)
 	}
 
-	defaultProjectID := s.BigQueryClient().Project()
+	defaultProjectID := s.BigQueryProject()
 	projectDescription := "The Google Cloud project ID containing the dataset."
 	datasetDescription := "The dataset to list table ids."
 	var datasetParameter tools.Parameter
