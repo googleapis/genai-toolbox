1737e7f1dfc4ab843dab5427b174cf1a3c416683
AnmolShukla2002 <anmol.28422@gmail.com>
2025-09-10 11:02:45 +0530

    docs: updated google api key conf

--- docs/en/getting-started/quickstart/js/genkit/quickstart.js
+++ docs/en/getting-started/quickstart/js/genkit/quickstart.js
@@ -2,8 +2,7 @@ import { ToolboxClient } from "@toolbox-sdk/core";
 import { genkit } from "genkit";
 import { googleAI } from '@genkit-ai/googleai';
 
-// Replace it with your API key
-process.env.GOOGLE_API_KEY = 'your-api-key';
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || 'your-api-key'; // Replace it with your API key
 
 const systemPrompt = `
 You're a helpful hotel assistant. You handle hotel searching, booking, and
