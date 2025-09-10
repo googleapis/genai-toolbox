3117c78af65a9ee9ab2d776ef87be05768346ce7
Prerna Kakkar <prernakakkar@google.com>
2025-09-10 12:59:28 +0000

    add base url check

--- internal/tools/cloudsql/cloudsqlwaitforoperation/cloudsqlwaitforoperation.go
+++ internal/tools/cloudsql/cloudsqlwaitforoperation/cloudsqlwaitforoperation.go
@@ -124,6 +124,11 @@ func (cfg Config) Initialize(srcs map[string]sources.Source) (tools.Tool, error)
 	if !ok {
 		return nil, fmt.Errorf("invalid source for %q tool: source kind must be `http`", kind)
 	}
+
+	if s.BaseURL != "https://sqladmin.googleapis.com" && !strings.HasPrefix(s.BaseURL, "http://127.0.0.1") {
+		return nil, fmt.Errorf("invalid source for %q tool: baseUrl must be `https://sqladmin.googleapis.com`", kind)
+	}
+
 	allParameters := tools.Parameters{
 		tools.NewStringParameter("project", "The project ID"),
 		tools.NewStringParameter("operation", "The operation ID"),
