d804ecb2b80c3161baf537e84a19e095c2454572
Sri Varshitha Bodala <srivarshitha@google.com>
2025-09-12 15:02:52 +0000

    Add User Agent

--- internal/tools/alloydb/alloydbcreatecluster/alloydbcreatecluster.go
+++ internal/tools/alloydb/alloydbcreatecluster/alloydbcreatecluster.go
@@ -155,6 +155,8 @@ func (t Tool) Invoke(ctx context.Context, params tools.ParamValues, accessToken 
         return nil, fmt.Errorf("error creating AlloyDB service: %w", err)
     }
 
+    alloydbService.UserAgent = t.Source.UserAgent
+
     urlString := fmt.Sprintf("projects/%s/locations/%s", projectId, locationId)
 
     // Build the request body using the type-safe Cluster struct.
