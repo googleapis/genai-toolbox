f6ceed248ef22293e5a1ace86a006a62535bed9f
Sri Varshitha Bodala <srivarshitha@google.com>
2025-09-12 16:06:51 +0000

    chore: Add user agent to alloydb control plane tools

--- internal/tools/alloydb/alloydblistclusters/alloydblistclusters.go
+++ internal/tools/alloydb/alloydblistclusters/alloydblistclusters.go
@@ -135,6 +135,8 @@ func (t Tool) Invoke(ctx context.Context, params tools.ParamValues, accessToken 
 		return nil, fmt.Errorf("error creating AlloyDB service: %w", err)
 	}
 
+	alloydbService.UserAgent = t.Source.UserAgent
+
 	urlString := fmt.Sprintf("projects/%s/locations/%s", projectId, locationId)
 
 	resp, err := alloydbService.Projects.Locations.Clusters.List(urlString).Do()
