44018ef8d58b17b43482ce9beba13ce9b6dcd696
Luka Fontanilla <maluka@google.com>
2025-09-17 20:18:53 +0000

    fix: unit tests conflated with integration test functions

--- internal/tools/looker/lookerhealthvacuum/lookerhealthvacuum_test.go
+++ internal/tools/looker/lookerhealthvacuum/lookerhealthvacuum_test.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//	http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,62 +12,102 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package lookerhealthvacuum
+package lookerhealthvacuum_test
 
 import (
-	"context"
-	"os"
-	"regexp"
+	"strings"
 	"testing"
-	"time"
 
-	"github.com/googleapis/genai-toolbox/internal/log"
+	yaml "github.com/goccy/go-yaml"
+	"github.com/google/go-cmp/cmp"
+	"github.com/googleapis/genai-toolbox/internal/server"
 	"github.com/googleapis/genai-toolbox/internal/testutils"
-	"github.com/googleapis/genai-toolbox/internal/util"
-	"github.com/googleapis/genai-toolbox/tests"
+	lhv "github.com/googleapis/genai-toolbox/internal/tools/looker/lookerhealthvacuum"
 )
 
-func TestLookerHealthVacuum(t *testing.T) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
-	defer cancel()
-
-	testLogger, err := log.NewStdLogger(os.Stdout, os.Stderr, "info")
+func TestParseFromYamlLookerHealthVacuum(t *testing.T) {
+	ctx, err := testutils.ContextWithNewLogger()
 	if err != nil {
 		t.Fatalf("unexpected error: %s", err)
 	}
-	ctx = util.WithLogger(ctx, testLogger)
-
-	var args []string
-
-	// Write config into a file and pass it to command
-
-toolsFile := map[string]any{
-		"tools": map[string]any{
-			"vacuum": map[string]any{
-				"kind":        "looker-health-vacuum",
-				"source":      "my-instance",
-				"description": "Vacuums the Looker instance.",
+	tcs := []struct {
+		desc string
+		in   string
+		want server.ToolConfigs
+	}{
+		{
+			desc: "basic example",
+			in: `
+			tools:
+				example_tool:
+					kind: looker-health-vacuum
+					source: my-instance
+					description: some description
+			`,
+			want: server.ToolConfigs{
+				"example_tool": lhv.Config{
+					Name:         "example_tool",
+					Kind:         "looker-health-vacuum",
+					Source:       "my-instance",
+					Description:  "some description",
+					AuthRequired: []string{},
+				},
 			},
 		},
 	}
-
-	cmd, cleanup, err := tests.StartCmd(ctx, toolsFile, args...)
-	if err != nil {
-		t.Fatalf("command initialization returned an error: %s", err)
+	for _, tc := range tcs {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := struct {
+				Tools server.ToolConfigs `yaml:"tools"`
+			}{}
+			// Parse contents
+			err := yaml.UnmarshalContext(ctx, testutils.FormatYaml(tc.in), &got)
+			if err != nil {
+				t.Fatalf("unable to unmarshal: %s", err)
+			}
+			if diff := cmp.Diff(tc.want, got.Tools); diff != "" {
+				t.Fatalf("incorrect parse: diff %v", diff)
+			}
+		})
 	}
-	defer cleanup()
+}
 
-	waitCtx, cancel := context.WithTimeout(ctx, 10*time.Second)
-	defer cancel()
-	out, err := testutils.WaitForString(waitCtx, regexp.MustCompile(`Server ready to serve`), cmd.Out)
+func TestFailParseFromYamlLookerHealthVacuum(t *testing.T) {
+	ctx, err := testutils.ContextWithNewLogger()
 	if err != nil {
-		t.Logf("toolbox command logs: \n%s", out)
-		t.Fatalf("toolbox didn't start successfully: %s", err)
+		t.Fatalf("unexpected error: %s", err)
 	}
-
-	// Test models action
-	tests.RunToolInvokeParametersTest(t, "vacuum", []byte(`{"action": "models", "project": "thelook_core", "model": "thelook"}`), "")
-
-	// Test explores action
-	tests.RunToolInvokeParametersTest(t, "vacuum", []byte(`{"action": "explores", "model": "thelook", "explore": "order_items"}`), "")
+	tcs := []struct {
+		desc string
+		in   string
+		err  string
+	}{
+		{
+			desc: "Invalid field",
+			in: `
+			tools:
+				example_tool:
+					kind: looker-health-vacuum
+					source: my-instance
+					invalid_field: true
+			`,
+			err: "unable to parse tool \"example_tool\" as kind \"looker-health-vacuum\": [2:1] unknown field \"invalid_field\"",
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := struct {
+				Tools server.ToolConfigs `yaml:"tools"`
+			}{}
+			// Parse contents
+			err := yaml.UnmarshalContext(ctx, testutils.FormatYaml(tc.in), &got)
+			if err == nil {
+				t.Fatalf("expect parsing to fail")
+			}
+			errStr := err.Error()
+			if !strings.Contains(errStr, tc.err) {
+				t.Fatalf("unexpected error string: got %q, want substring %q", errStr, tc.err)
+			}
+		})
+	}
 }
