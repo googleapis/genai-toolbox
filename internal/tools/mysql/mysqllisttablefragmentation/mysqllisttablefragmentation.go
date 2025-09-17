892b301b7568febe1162aa4b92657001efe1d6a5
shuzhou-gc <zhoushu@google.com>
2025-09-17 15:54:31 -0700

    adopt to the new coverage calc
    Co-authored-by: Yuan Teoh <45984206+Yuan325@users.noreply.github.com>

--- internal/tools/mysql/mysqllisttablefragmentation/mysqllisttablefragmentation.go
+++ internal/tools/mysql/mysqllisttablefragmentation/mysqllisttablefragmentation.go
@@ -72,6 +72,7 @@ type compatibleSource interface {
 
 // validate compatible sources are still compatible
 var _ compatibleSource = &mysql.Source{}
+var _ compatibleSource = &cloudsqlmysql.Source{}
 
 var compatibleSources = [...]string{mysql.SourceKind, cloudsqlmysql.SourceKind}
 
