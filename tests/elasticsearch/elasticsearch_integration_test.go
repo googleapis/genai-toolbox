b03f8fee856ae4fc286408d10072e59cb86db0db
Laurent Saint-Félix <laurent.saintfelix@elastic.co>
2025-09-18 18:26:08 +0200

    feat(elasticsearch): refactor test setup and enhance tool configurations for Elasticsearch integration

--- tests/elasticsearch/elasticsearch_integration_test.go
+++ tests/elasticsearch/elasticsearch_integration_test.go
@@ -15,13 +15,9 @@
 package elasticsearch
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
-	"io"
-	"net/http"
+	"fmt"
 	"os"
-	"reflect"
 	"regexp"
 	"strings"
 	"testing"
@@ -30,13 +26,13 @@ import (
 	"github.com/elastic/go-elasticsearch/v8"
 	"github.com/elastic/go-elasticsearch/v8/esapi"
 
-	mcp "github.com/googleapis/genai-toolbox/internal/server/mcp/jsonrpc"
 	"github.com/googleapis/genai-toolbox/internal/testutils"
 	"github.com/googleapis/genai-toolbox/tests"
 )
 
 var (
 	ElasticsearchSourceKind = "elasticsearch"
+	ElasticsearchToolKind   = "elasticsearch-esql"
 	EsAddress               = os.Getenv("ELASTICSEARCH_HOST")
 	EsUser                  = os.Getenv("ELASTICSEARCH_USER")
 	EsPass                  = os.Getenv("ELASTICSEARCH_PASS")
@@ -49,48 +45,26 @@ func getElasticsearchVars(t *testing.T) map[string]any {
 	return map[string]any{
 		"kind":      ElasticsearchSourceKind,
 		"addresses": []string{EsAddress},
-		"user":      EsUser,
+		"username":  EsUser,
 		"password":  EsPass,
 	}
 }
 
 func TestElasticsearchToolEndpoints(t *testing.T) {
-	sourceConfig := getElasticsearchVars(t)
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
 	var args []string
 
-	tools := map[string]any{
-		"sources": map[string]any{
-			"my-elasticsearch-instance": sourceConfig,
-		},
-		"tools": map[string]any{
-			"esql-tool": map[string]any{
-				"kind":        "elasticsearch-esql",
-				"source":      "my-elasticsearch-instance",
-				"description": "Elasticsearch ES|QL tool",
-				"query": `FROM test-index
-                         | KEEP first_name, last_name`,
-			},
-			"esql-with-params-tool": map[string]any{
-				"kind":        "elasticsearch-esql",
-				"source":      "my-elasticsearch-instance",
-				"description": "Elasticsearch ES|QL tool with parameters",
-				"query":       `FROM test-index | LIMIT ?limit`,
-				"parameters": []any{
-					map[string]any{
-						"name":        "limit",
-						"type":        "integer",
-						"description": "limit the number of results",
-						"required":    true,
-					},
-				},
-			},
-		},
-	}
+	sourceConfig := getElasticsearchVars(t)
 
-	cmd, cleanup, err := tests.StartCmd(ctx, tools, args...)
+	index := "test-index"
+
+	paramToolStatement, idParamToolStatement, nameParamToolStatement, arrayParamToolStatement, authToolStatement := getElasticsearchQueries(index)
+
+	toolsConfig := getElasticsearchToolsConfig(sourceConfig, ElasticsearchToolKind, paramToolStatement, idParamToolStatement, nameParamToolStatement, arrayParamToolStatement, authToolStatement)
+
+	cmd, cleanup, err := tests.StartCmd(ctx, toolsConfig, args...)
 	if err != nil {
 		t.Fatalf("failed to start cmd: %v", err)
 	}
@@ -116,235 +90,201 @@ func TestElasticsearchToolEndpoints(t *testing.T) {
 	// Delete index if already exists
 	defer func() {
 		_, err = esapi.IndicesDeleteRequest{
-			Index: []string{"test-index"},
+			Index: []string{index},
 		}.Do(ctx, esClient)
 		if err != nil {
 			t.Fatalf("error deleting index: %s", err)
 		}
 	}()
 
+	alice := fmt.Sprintf(`{
+									"id": 1,
+									"name": "Alice",
+									"email": "%s"
+								}`, tests.ServiceAccountEmail)
+
 	// Index sample documents
 	sampleDocs := []string{
-		`{"first_name": "John", "last_name": "Doe"}`,
-		`{"first_name": "Jane", "last_name": "Smith"}`,
+		alice,
+		`{"id": 2, "name": "Jane", "email": "janedoe@gmail.com"}`,
+		`{"id": 3, "name": "Sid"}`,
+		`{"id": 4, "name": "null"}`,
 	}
 	for _, doc := range sampleDocs {
-		_, err := esapi.IndexRequest{
+		res, err := esapi.IndexRequest{
 			Index:   "test-index",
 			Body:    strings.NewReader(doc),
 			Refresh: "true",
 		}.Do(ctx, esClient)
+		if res.IsError() {
+			t.Fatalf("error indexing document: %s", res.String())
+		}
 		if err != nil {
 			t.Fatalf("error indexing document: %s", err)
 		}
 	}
 
-	// Test tool get endpoint
-	tcs := []struct {
-		name string
-		api  string
-		want map[string]any
-	}{
-		{
-			name: "Get Elasticsearch ES|QL tool",
-			api:  "http://127.0.0.1:5000/api/tool/esql-tool/",
-			want: map[string]any{
-				"esql-tool": map[string]any{
-					"description":  "Elasticsearch ES|QL tool",
-					"parameters":   []any{},
-					"authRequired": []any{},
+	// Get configs for tests
+	select1Want, myToolId3NameAliceWant, myToolById4Want, nullWant, mcpMyToolId3NameAliceWant := getElasticsearchWants()
+
+	tests.RunToolGetTest(t)
+	tests.RunToolInvokeTest(t, select1Want,
+		tests.DisableArrayTest(),
+
+		tests.WithMyToolId3NameAliceWant(myToolId3NameAliceWant),
+		tests.WithMyToolById4Want(myToolById4Want),
+		tests.WithNullWant(nullWant),
+	)
+	tests.RunMCPToolCallMethod(t, "", "", tests.WithMcpMyToolId3NameAliceWant(mcpMyToolId3NameAliceWant))
+}
+
+func getElasticsearchQueries(index string) (string, string, string, string, string) {
+	paramToolStatement := fmt.Sprintf(`FROM %s | WHERE id == ?id OR name == ?name | SORT id ASC`, index)
+	idParamToolStatement := fmt.Sprintf(`FROM %s | WHERE id == ?id`, index)
+	nameParamToolStatement := fmt.Sprintf(`FROM %s | WHERE name == ?name`, index)
+	arrayParamToolStatement := fmt.Sprintf(`FROM %s | WHERE first_name == ?first_name_array`, index) // Not supported yet.
+	authToolStatement := fmt.Sprintf(`FROM %s | WHERE name == ?name`, index)
+	return paramToolStatement, idParamToolStatement, nameParamToolStatement, arrayParamToolStatement, authToolStatement
+}
+
+func getElasticsearchWants() (string, string, string, string, string) {
+	select1Want := `[["test@elastic.co","test@elastic.co",1,"Alice","Alice"],["janedoe@gmail.com","janedoe@gmail.com",2,"Jane","Jane"],[null,null,3,"Sid","Sid"],[null,null,4,"null","null"]]`
+	myToolId3NameAliceWant := `[["test@elastic.co","test@elastic.co",1,"Alice","Alice"],[null,null,3,"Sid","Sid"]]`
+	myToolById4Want := `[[null,null,4,"null","null"]]`
+	nullWant := `{"error":{"root_cause":[{"type":"verification_exception","reason":"Found 1 problem\nline 1:25: first argument of [name == ?name] is [text] so second argument must also be [text] but was [null]"}],"type":"verification_exception","reason":"Found 1 problem\nline 1:25: first argument of [name == ?name] is [text] so second argument must also be [text] but was [null]"},"status":400}`
+
+	mcpSelect1Want := `{"jsonrpc":"2.0","id":"my-tool","result":{"content":[{"type":"text","text":"[[\"test@elastic.co\",\"test@elastic.co\",1,\"Alice\",\"Alice\"],[null,null,3,\"Sid\",\"Sid\"]]"}]}}`
+
+	return select1Want, myToolId3NameAliceWant, myToolById4Want, nullWant, mcpSelect1Want
+}
+
+func getElasticsearchToolsConfig(sourceConfig map[string]any, toolKind, paramToolStatement, idParamToolStmt, nameParamToolStmt, arrayToolStatement, authToolStatement string) map[string]any {
+	toolsFile := map[string]any{
+		"sources": map[string]any{
+			"my-instance": sourceConfig,
+		},
+		"authServices": map[string]any{
+			"my-google-auth": map[string]any{
+				"kind":     "google",
+				"clientId": tests.ClientId,
+			},
+		},
+		"tools": map[string]any{
+			"my-simple-tool": map[string]any{
+				"kind":        toolKind,
+				"source":      "my-instance",
+				"description": "Simple tool to test end to end functionality.",
+				"query":       "FROM test-index | SORT id ASC",
+			},
+			"my-tool": map[string]any{
+				"kind":        toolKind,
+				"source":      "my-instance",
+				"description": "Tool to test invocation with params.",
+				"query":       paramToolStatement,
+				"parameters": []any{
+					map[string]any{
+						"name":        "id",
+						"type":        "integer",
+						"description": "user ID",
+					},
+					map[string]any{
+						"name":        "name",
+						"type":        "string",
+						"description": "user name",
+					},
 				},
+			},
+			"my-tool-by-id": map[string]any{
+				"kind":        toolKind,
+				"source":      "my-instance",
+				"description": "Tool to test invocation with params.",
+				"query":       idParamToolStmt,
+				"parameters": []any{
+					map[string]any{
+						"name":        "id",
+						"type":        "integer",
+						"description": "user ID",
+					},
+				},
+			},
+			"my-tool-by-name": map[string]any{
+				"kind":        toolKind,
+				"source":      "my-instance",
+				"description": "Tool to test invocation with params.",
+				"query":       nameParamToolStmt,
+				"parameters": []any{
+					map[string]any{
+						"name":        "name",
+						"type":        "string",
+						"description": "user name",
+						"required":    false,
+					},
+				},
+			},
+			"my-array-tool": map[string]any{
+				"kind":        toolKind,
+				"source":      "my-instance",
+				"description": "Tool to test invocation with array params.",
+				"query":       arrayToolStatement,
+				"parameters": []any{
+					map[string]any{
+						"name":        "idArray",
+						"type":        "array",
+						"description": "ID array",
+						"items": map[string]any{
+							"name":        "id",
+							"type":        "integer",
+							"description": "ID",
+						},
+					},
+					map[string]any{
+						"name":        "nameArray",
+						"type":        "array",
+						"description": "user name array",
+						"items": map[string]any{
+							"name":        "name",
+							"type":        "string",
+							"description": "user name",
+						},
+					},
+				},
+			},
+			"my-auth-tool": map[string]any{
+				"kind":        toolKind,
+				"source":      "my-instance",
+				"description": "Tool to test authenticated parameters.",
+				// statement to auto-fill authenticated parameter
+				"query": authToolStatement,
+				"parameters": []map[string]any{
+					{
+						"name":        "email",
+						"type":        "string",
+						"description": "user email",
+						"authServices": []map[string]string{
+							{
+								"name":  "my-google-auth",
+								"field": "email",
+							},
+						},
+					},
+				},
+			},
+			"my-auth-required-tool": map[string]any{
+				"kind":        toolKind,
+				"source":      "my-instance",
+				"description": "Tool to test auth required invocation.",
+				"query":       "FROM *",
+				"authRequired": []string{
+					"my-google-auth",
+				},
+			},
+			"my-fail-tool": map[string]any{
+				"kind":        toolKind,
+				"source":      "my-instance",
+				"description": "Tool to test statement with incorrect syntax.",
+				"query":       "SELEC 1;",
 			},
 		},
 	}
-	for _, tc := range tcs {
-		t.Run(tc.name, func(t *testing.T) {
-			resp, err := http.Get(tc.api)
-			if err != nil {
-				t.Fatalf("error when sending a request: %s", err)
-			}
-			defer resp.Body.Close()
-			if resp.StatusCode != 200 {
-				t.Fatalf("response status code is not 200")
-			}
-
-			var body map[string]interface{}
-			err = json.NewDecoder(resp.Body).Decode(&body)
-			if err != nil {
-				t.Fatalf("error parsing response body")
-			}
-
-			got, ok := body["tools"]
-			if !ok {
-				t.Fatalf("unable to find tools in response body")
-			}
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Fatalf("got %q, want %q", got, tc.want)
-			}
-		})
-	}
-
-	// Test tool invoke endpoint
-	invokeTcs := []struct {
-		name        string
-		api         string
-		requestBody io.Reader
-		want        string
-	}{
-		{
-			name:        "Invoke Elasticsearch ES|QL tool",
-			api:         "http://127.0.0.1:5000/api/tool/esql-tool/invoke",
-			requestBody: strings.NewReader("{}"),
-			want:        `[["John","Doe"],["Jane","Smith"]]`,
-		},
-	}
-	for _, tc := range invokeTcs {
-		t.Run(tc.name, func(t *testing.T) {
-			resp, err := http.Post(tc.api, "application/json", tc.requestBody)
-			if err != nil {
-				t.Fatalf("error when sending a request: %s", err)
-			}
-			defer resp.Body.Close()
-			if resp.StatusCode != http.StatusOK {
-				bodyBytes, _ := io.ReadAll(resp.Body)
-				t.Fatalf("response status code is not 200, got %d: %s", resp.StatusCode, string(bodyBytes))
-			}
-
-			var body map[string]interface{}
-			err = json.NewDecoder(resp.Body).Decode(&body)
-			if err != nil {
-				t.Fatalf("error parsing response body")
-			}
-			got, ok := body["result"].(string)
-
-			if !ok {
-				t.Fatalf("unable to find result in response body")
-			}
-
-			if !strings.Contains(got, tc.want) {
-				t.Fatalf("Expected substring not found:\ngot:  %q\nwant: %q (to be contained within got)", got, tc.want)
-			}
-		})
-	}
-
-	// Test tool invoke endpoint
-	invokeMcpTcs := []struct {
-		name          string
-		api           string
-		requestBody   mcp.JSONRPCRequest
-		requestHeader map[string]string
-		want          string
-	}{
-		{
-			name:          "MCP Invoke ES|QL tool",
-			api:           "http://127.0.0.1:5000/mcp",
-			requestHeader: map[string]string{},
-			requestBody: mcp.JSONRPCRequest{
-				Jsonrpc: "2.0",
-				Id:      "esql-tool",
-				Request: mcp.Request{
-					Method: "tools/call",
-				},
-				Params: map[string]any{
-					"name":      "esql-tool",
-					"arguments": map[string]any{},
-				},
-			},
-			want: "[[\\\"John\\\",\\\"Doe\\\"],[\\\"Jane\\\",\\\"Smith\\\"]]",
-		},
-	}
-	for _, tc := range invokeMcpTcs {
-		t.Run(tc.name, func(t *testing.T) {
-			reqMarshal, err := json.Marshal(tc.requestBody)
-			if err != nil {
-				t.Fatalf("unexpected error during marshaling of request body")
-			}
-			// Send Tool invocation request
-			req, err := http.NewRequest(http.MethodPost, tc.api, bytes.NewBuffer(reqMarshal))
-			if err != nil {
-				t.Fatalf("unable to create request: %s", err)
-			}
-			req.Header.Add("Content-type", "application/json")
-			for k, v := range tc.requestHeader {
-				req.Header.Add(k, v)
-			}
-			resp, err := http.DefaultClient.Do(req)
-			if err != nil {
-				t.Fatalf("unable to send request: %s", err)
-			}
-			respBody, err := io.ReadAll(resp.Body)
-			if err != nil {
-				t.Fatalf("unable to read request body: %s", err)
-			}
-			defer resp.Body.Close()
-			got := string(bytes.TrimSpace(respBody))
-
-			if !strings.Contains(got, tc.want) {
-				t.Fatalf("Expected substring not found:\ngot:  %q\nwant: %q (to be contained within got)", got, tc.want)
-			}
-		})
-	}
-
-	// Test tool invoke endpoint
-	invokeWithTemplateTcs := []struct {
-		name          string
-		insert        bool
-		api           string
-		requestHeader map[string]string
-		requestBody   io.Reader
-		want          string
-		isErr         bool
-	}{
-		{
-			name:          "Invoke ES|QL tool with parameters (sort ascending)",
-			api:           "http://127.0.0.1:5000/api/tool/esql-with-params-tool/invoke",
-			requestHeader: map[string]string{},
-			requestBody:   bytes.NewBuffer([]byte("{\"limit\": 1}")),
-			want:          "[[\"John\",\"John\",\"Doe\",\"Doe\"]]",
-			isErr:         false,
-		},
-	}
-	for _, tc := range invokeWithTemplateTcs {
-		t.Run(tc.name, func(t *testing.T) {
-			// Send Tool invocation request
-			req, err := http.NewRequest(http.MethodPost, tc.api, tc.requestBody)
-			if err != nil {
-				t.Fatalf("unable to create request: %s", err)
-			}
-			req.Header.Add("Content-type", "application/json")
-			for k, v := range tc.requestHeader {
-				req.Header.Add(k, v)
-			}
-
-			resp, err := http.DefaultClient.Do(req)
-			if err != nil {
-				t.Fatalf("unable to send request: %s", err)
-			}
-			defer resp.Body.Close()
-
-			if resp.StatusCode != http.StatusOK {
-				if tc.isErr {
-					return
-				}
-				bodyBytes, _ := io.ReadAll(resp.Body)
-				t.Fatalf("response status code is not 200, got %d: %s", resp.StatusCode, string(bodyBytes))
-			}
-
-			// Check response body
-			var body map[string]interface{}
-			err = json.NewDecoder(resp.Body).Decode(&body)
-			if err != nil {
-				t.Fatalf("error parsing response body")
-			}
-
-			got, ok := body["result"].(string)
-			if !ok {
-				t.Fatalf("unable to find result in response body")
-			}
-
-			if !strings.Contains(got, tc.want) {
-				t.Fatalf("Expected substring not found:\ngot:  %q\nwant: %q (to be contained within got)", got, tc.want)
-			}
-		})
-	}
+	return toolsFile
 }
