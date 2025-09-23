a73dc9960376317ebdef4ffd6ecafb35946c37cb
Harsh Jha <harshrjha@google.com>
2025-09-23 17:29:44 +0530

    chore: modified sql file path

--- .ci/quickstart_test/run_go_tests.sh
+++ .ci/quickstart_test/run_go_tests.sh
@@ -18,7 +18,7 @@ set -e
 
 TABLE_NAME="hotels_go"
 QUICKSTART_GO_DIR="docs/en/getting-started/quickstart/go"
-SQL_FILE=".ci/setup_hotels_sample.sql"
+SQL_FILE=".ci/quickstart_test/setup_hotels_sample.sql"
 
 PROXY_PID=""
 TOOLBOX_PID=""
