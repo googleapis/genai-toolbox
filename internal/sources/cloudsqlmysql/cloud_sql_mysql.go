be35382dec2646054a4e0b63e8c9068e58c16acd
Disha Prakash <dishaprakash@google.com>
2025-12-02 08:05:45 +0000

    update dsn

--- internal/sources/cloudsqlmysql/cloud_sql_mysql.go
+++ internal/sources/cloudsqlmysql/cloud_sql_mysql.go
@@ -158,7 +158,7 @@ func initCloudSQLMySQLConnectionPool(ctx context.Context, tracer trace.Tracer, n
 	// Tell the driver to use the Cloud SQL Go Connector to create connections
 	if useIAM {
 		// Set allowCleartextPasswords to true for IAM
-		dsn = fmt.Sprintf("%s:%s@cloudsql-mysql(%s:%s:%s)/%s?connectionAttributes=program_name:%s&allowCleartextPasswords=true&tls=true",
+		dsn = fmt.Sprintf("%s:%s@cloudsql-mysql(%s:%s:%s)/%s?connectionAttributes=program_name:%s&allowCleartextPasswords=true",
 			user,
 			pass,
 			project,
