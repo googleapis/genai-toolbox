cd39b9579cd47cc0997c6cf24e419cd1027e9680
Wenxin Du <117315983+duwenxin99@users.noreply.github.com>
2025-12-04 12:19:55 -0500

    ci(cloudsqlmysql): fix Cloud SQL MySQL test failure (#2105)

--- tests/tool.go
+++ tests/tool.go
@@ -2097,7 +2097,14 @@ func RunPostgresListSequencesTest(t *testing.T, ctx context.Context, pool *pgxpo
 }
 
 // RunMySQLListTablesTest run tests against the mysql-list-tables tool
-func RunMySQLListTablesTest(t *testing.T, databaseName, tableNameParam, tableNameAuth string) {
+func RunMySQLListTablesTest(t *testing.T, databaseName, tableNameParam, tableNameAuth, expectedOwner string) {
+	var ownerWant any
+	if expectedOwner == "" {
+		ownerWant = nil
+	} else {
+		ownerWant = expectedOwner
+	}
+
 	type tableInfo struct {
 		ObjectName    string `json:"object_name"`
 		SchemaName    string `json:"schema_name"`
@@ -2129,6 +2136,7 @@ func RunMySQLListTablesTest(t *testing.T, databaseName, tableNameParam, tableNam
 		ObjectName: tableNameParam,
 		SchemaName: databaseName,
 		ObjectType: "TABLE",
+		Owner:      ownerWant,
 		Columns: []column{
 			{DataType: "int", ColumnName: "id", IsNotNullable: 1, OrdinalPosition: 1},
 			{DataType: "varchar(255)", ColumnName: "name", OrdinalPosition: 2},
@@ -2142,6 +2150,7 @@ func RunMySQLListTablesTest(t *testing.T, databaseName, tableNameParam, tableNam
 		ObjectName: tableNameAuth,
 		SchemaName: databaseName,
 		ObjectType: "TABLE",
+		Owner:      ownerWant,
 		Columns: []column{
 			{DataType: "int", ColumnName: "id", IsNotNullable: 1, OrdinalPosition: 1},
 			{DataType: "varchar(255)", ColumnName: "name", OrdinalPosition: 2},
