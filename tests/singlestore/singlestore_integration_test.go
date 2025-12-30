e66deb9abf6cf79c239e1bdb24bd86266fd95c7f
rahulpinto19 <rahulyadav252424@gmail.com>
2025-12-30 08:55:12 +0000

    tests

--- tests/singlestore/singlestore_integration_test.go
+++ tests/singlestore/singlestore_integration_test.go
@@ -108,7 +108,7 @@ func setupSingleStoreTable(t *testing.T, ctx context.Context, pool *sql.DB, crea
 	if err != nil {
 		t.Fatalf("unable to connect to test database: %s", err)
 	}
-	
+
 	// Safety drop before creation
 	_, err = pool.ExecContext(ctx, fmt.Sprintf("DROP TABLE IF EXISTS %s;", tableName))
 	if err != nil {
