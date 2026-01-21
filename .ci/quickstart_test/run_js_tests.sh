40b95f9552699badf0648d51046a082ab1dd2c4a
AnmolShukla2002 <anmol.28422@gmail.com>
2026-01-21 14:43:20 +0530

    check for internal reigstry redirection

--- .ci/quickstart_test/run_js_tests.sh
+++ .ci/quickstart_test/run_js_tests.sh
@@ -80,12 +80,21 @@ run_orch_test() {
 
     echo "--- Debugging npm config for $orch_name ---"
     npm config list
-    
+
+    echo "--- Active Registry for $orch_name ---"
+    npm config get registry
+
+    echo "--- Inspecting .npmrc files ---"
+    [ -f ".npmrc" ] && echo "Local .npmrc content:" && cat .npmrc
+    [ -f "$HOME/.npmrc" ] && echo "Global .npmrc content:" && cat "$HOME/.npmrc"
+
+    export GPKG_DEBUG=1
+
     echo "Installing dependencies for $orch_name..."
     if [ -f "package-lock.json" ]; then
-      npm ci
+      npm ci --loglevel verbose
     else
-      npm install
+      npm install --loglevel verbose
     fi
 
     cd ..
