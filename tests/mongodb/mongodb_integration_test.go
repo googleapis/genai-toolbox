b0927d6295877818947922d01903c2aa6b1548ac
Dennis Geurts <dennisg@dennisg.nl>
2026-01-22 09:07:23 +0100

    fix: mongodb driver version bump to v2

--- tests/mongodb/mongodb_integration_test.go
+++ tests/mongodb/mongodb_integration_test.go
@@ -28,8 +28,8 @@ import (
 
 	"github.com/googleapis/genai-toolbox/internal/testutils"
 	"github.com/googleapis/genai-toolbox/tests"
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/v2/mongo"
+	"go.mongodb.org/mongo-driver/v2/mongo/options"
 )
 
 var (
