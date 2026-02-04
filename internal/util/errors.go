1f9cd1b134dc60ea57a94cd72b93fabeffbefbe7
duwenxin99 <duwenxin@google.com>
2026-02-04 14:40:37 -0500

    nit

--- internal/util/errors.go
+++ internal/util/errors.go
@@ -47,8 +47,6 @@ func NewAgentError(msg string, cause error) *AgentError {
 	return &AgentError{Msg: msg, Cause: cause}
 }
 
-var _ ToolboxError = &AgentError{}
-
 // ClientServerError returns 4XX/5XX error code
 type ClientServerError struct {
 	Msg   string
