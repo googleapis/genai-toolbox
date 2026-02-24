b70b9684b089e3d2e98da22288ccadaef7a9b567
Yuan Teoh <yuanteoh@google.com>
2026-02-23 16:57:25 -0800

    resolve gemini comments

--- cmd/internal/serve/command_test.go
+++ cmd/internal/serve/command_test.go
@@ -19,6 +19,7 @@ import (
 	"context"
 	"strings"
 	"testing"
+	"time"
 
 	"github.com/googleapis/genai-toolbox/cmd/internal"
 	"github.com/spf13/cobra"
@@ -42,6 +43,10 @@ func serveCommand(ctx context.Context, args []string) (string, error) {
 }
 
 func TestServe(t *testing.T) {
+	// context will automatically shutdown in 1 second.
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
 	args := []string{"serve", "--port", "0"}
 	output, err := serveCommand(ctx, args)
 	if err != nil {
