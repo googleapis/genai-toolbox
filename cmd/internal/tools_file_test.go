06901efa484cc87ef5e3d510073660d318373eb1
Anuj Jhunjhunwala <anujjhunjhunwala98@gmail.com>
2026-03-24 10:30:12 +0000

    temp remove tool file test changes

--- cmd/internal/tools_file_test.go
+++ cmd/internal/tools_file_test.go
@@ -1848,7 +1848,7 @@ func TestPrebuiltTools(t *testing.T) {
 			wantToolset: server.ToolsetConfigs{
 				"discovery": tools.ToolsetConfig{
 					Name:      "discovery",
-					ToolNames: []string{"search_entries", "lookup_entry", "search_aspect_types", "lookup_context"},
+					ToolNames: []string{"search_entries", "lookup_entry", "search_aspect_types"},
 				},
 			},
 		},
