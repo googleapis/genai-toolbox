40cef0c15c35c33ed45abb47e9d2b84f8ebaa00d
Yuan Teoh <45984206+Yuan325@users.noreply.github.com>
2026-05-11 10:01:08 -0700

    chore: update Tool interface (#3166)
    Update `type Tool interface{}` with the following:
    * Add GetName()
    * Add GetDescription()
    * Add GetAuthRequired()
    * Add GetAnnotations()
    * Remove McpManifest()
    
    The new functions are needed when versionizing the mcpManifest
    generation since it will require those values.
    
    This PR is expected to fail the unit test. Unit test will be resolved in
    the next PR. #3167

--- internal/tools/looker/lookerhealthvacuum/lookerhealthvacuum.go
+++ internal/tools/looker/lookerhealthvacuum/lookerhealthvacuum.go
@@ -91,16 +91,6 @@ func (cfg Config) Initialize(srcs map[string]sources.Source) (tools.Tool, error)
 		minQueriesParameter,
 	}
 
-	annotations := cfg.Annotations
-	if annotations == nil {
-		readOnlyHint := true
-		annotations = &tools.ToolAnnotations{
-			ReadOnlyHint: &readOnlyHint,
-		}
-	}
-
-	mcpManifest := tools.GetMcpManifest(cfg.Name, cfg.Description, cfg.AuthRequired, params, annotations)
-
 	return Tool{
 		Config:     cfg,
 		Parameters: params,
@@ -109,7 +99,6 @@ func (cfg Config) Initialize(srcs map[string]sources.Source) (tools.Tool, error)
 			Parameters:   params.Manifest(),
 			AuthRequired: cfg.AuthRequired,
 		},
-		mcpManifest: mcpManifest,
 	}, nil
 }
 
@@ -117,9 +106,31 @@ var _ tools.Tool = Tool{}
 
 type Tool struct {
 	Config
-	Parameters  parameters.Parameters
-	manifest    tools.Manifest
-	mcpManifest tools.McpManifest
+	Parameters parameters.Parameters
+	manifest   tools.Manifest
+}
+
+func (t Tool) GetName() string {
+	return t.Name
+}
+
+func (t Tool) GetDescription() string {
+	return t.Description
+}
+
+func (t Tool) GetAuthRequired() []string {
+	return t.AuthRequired
+}
+
+func (t Tool) GetAnnotations() *tools.ToolAnnotations {
+	annotations := t.Annotations
+	if annotations == nil {
+		readOnlyHint := true
+		annotations = &tools.ToolAnnotations{
+			ReadOnlyHint: &readOnlyHint,
+		}
+	}
+	return annotations
 }
 
 func (t Tool) ToConfig() tools.ToolConfig {
@@ -186,10 +197,6 @@ func (t Tool) EmbedParams(ctx context.Context, paramValues parameters.ParamValue
 
 func (t Tool) Manifest() tools.Manifest {
 	return t.manifest
-}
-
-func (t Tool) McpManifest() tools.McpManifest {
-	return t.mcpManifest
 }
 
 func (t Tool) RequiresClientAuthorization(resourceMgr tools.SourceProvider) (bool, error) {
--- internal/tools/looker/lookerswitchgitbranch/lookerswitchgitbranch.go
+++ internal/tools/looker/lookerswitchgitbranch/lookerswitchgitbranch.go
@@ -74,17 +74,6 @@ func (cfg Config) Initialize(srcs map[string]sources.Source) (tools.Tool, error)
 	refParameter := parameters.NewStringParameterWithDefault("ref", "", "The ref to switch the branch to using `reset --hard`.")
 	params := parameters.Parameters{projectIdParameter, branchParameter, refParameter}
 
-	annotations := cfg.Annotations
-	if annotations == nil {
-		annotations = &tools.ToolAnnotations{}
-	}
-	readOnlyHint := false
-	annotations.ReadOnlyHint = &readOnlyHint
-	destructiveHint := true
-	annotations.DestructiveHint = &destructiveHint
-
-	mcpManifest := tools.GetMcpManifest(cfg.Name, cfg.Description, cfg.AuthRequired, params, annotations)
-
 	// finish tool setup
 	return Tool{
 		Config:     cfg,
@@ -94,7 +83,6 @@ func (cfg Config) Initialize(srcs map[string]sources.Source) (tools.Tool, error)
 			Parameters:   params.Manifest(),
 			AuthRequired: cfg.AuthRequired,
 		},
-		mcpManifest: mcpManifest,
 	}, nil
 }
 
@@ -103,9 +91,32 @@ var _ tools.Tool = Tool{}
 
 type Tool struct {
 	Config
-	Parameters  parameters.Parameters `yaml:"parameters"`
-	manifest    tools.Manifest
-	mcpManifest tools.McpManifest
+	Parameters parameters.Parameters `yaml:"parameters"`
+	manifest   tools.Manifest
+}
+
+func (t Tool) GetName() string {
+	return t.Name
+}
+
+func (t Tool) GetDescription() string {
+	return t.Description
+}
+
+func (t Tool) GetAuthRequired() []string {
+	return t.AuthRequired
+}
+
+func (t Tool) GetAnnotations() *tools.ToolAnnotations {
+	annotations := t.Annotations
+	if annotations == nil {
+		annotations = &tools.ToolAnnotations{}
+	}
+	readOnlyHint := false
+	annotations.ReadOnlyHint = &readOnlyHint
+	destructiveHint := true
+	annotations.DestructiveHint = &destructiveHint
+	return annotations
 }
 
 func (t Tool) ToConfig() tools.ToolConfig {
@@ -150,10 +161,6 @@ func (t Tool) EmbedParams(ctx context.Context, paramValues parameters.ParamValue
 
 func (t Tool) Manifest() tools.Manifest {
 	return t.manifest
-}
-
-func (t Tool) McpManifest() tools.McpManifest {
-	return t.mcpManifest
 }
 
 func (t Tool) RequiresClientAuthorization(resourceMgr tools.SourceProvider) (bool, error) {
