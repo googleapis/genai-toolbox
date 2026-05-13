b617902873d97a8dd20cfc6a0f158740b7ef2f59
Yuan Teoh <45984206+Yuan325@users.noreply.github.com>
2026-05-13 15:35:01 -0700

    Apply suggestions from code review
    Co-authored-by: Wenxin Du <117315983+duwenxin99@users.noreply.github.com>

--- internal/server/mcp/v20250326/manifests.go
+++ internal/server/mcp/v20250326/manifests.go
@@ -42,7 +42,7 @@ func generateToolManifest(name, desc string, params parameters.Parameters, annot
 	return mcpManifest
 }
 
-// generateToolManifest generates the input schema and get authParam
+// generateParamManifest generates the input schema and get authParam
 func generateParamManifest(ps parameters.Parameters) (InputSchema, map[string][]string) {
 	properties := make(map[string]parameters.ParameterMcpManifest)
 	required := make([]string, 0)
