1922c5226cafdabf266cae5101fefe256d5d3ef6
duwenxin99 <duwenxin@google.com>
2026-05-18 17:35:38 -0400

    update comment

--- internal/sources/mssql/mssql.go
+++ internal/sources/mssql/mssql.go
@@ -117,7 +117,7 @@ func (s *Source) RunSQL(ctx context.Context, statement string, params []any) (an
 	cols, err := results.Columns()
 	// If Columns() errors, it might be a DDL/DML without an OUTPUT clause.
 	// We proceed, and results.Err() will catch actual query execution errors.
-	// 'out' will remain nil if cols is empty or err is not nil here.
+	// 'out' will remain an empty slice if cols is empty or err is not nil here.
 	out := []any{}
 	if err == nil && len(cols) > 0 {
 		// create an array of values for each column, which can be re-used to scan each row
