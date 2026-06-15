59b777475dc2d323734e7713e4a3fc1f03a04ef5
Anuj Jhunjhunwala <anujjhunjhunwala98@gmail.com>
2026-06-15 08:19:36 +0000

    Add unit test

--- internal/sources/dataplex/dataplex.go
+++ internal/sources/dataplex/dataplex.go
@@ -123,11 +123,6 @@ func initDataplexConnection(
 	ctx, span := sources.InitConnectionSpan(ctx, tracer, SourceType, name)
 	defer span.End()
 
-	cred, err := google.FindDefaultCredentials(ctx, sources.CloudPlatformScope)
-	if err != nil {
-		return nil, nil, fmt.Errorf("failed to find default Google Cloud credentials for project %q: %w", project, err)
-	}
-
 	userAgent, err := util.UserAgentFromContext(ctx)
 	if err != nil {
 		return nil, nil, err
