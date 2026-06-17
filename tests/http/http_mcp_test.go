48de99e5a1affbe7dfbab249ddcdefdc4f943905
duwenxin99 <duwenxin@google.com>
2026-06-17 16:40:02 -0400

    test(source/http): allow loopback connections for test servers

--- tests/http/http_mcp_test.go
+++ tests/http/http_mcp_test.go
@@ -424,8 +424,9 @@ func getMCPHTTPSourceConfig(t *testing.T) map[string]any {
 	idToken = "Bearer " + idToken
 
 	return map[string]any{
-		"type":    HttpSourceType,
-		"headers": map[string]string{"Authorization": idToken},
+		"type":                 HttpSourceType,
+		"headers":              map[string]string{"Authorization": idToken},
+		"allowPrivateNetworks": true,
 	}
 }
 
