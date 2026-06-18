7073c98165827888e9e68228972df7627ca625ef
Yuan Teoh <yuanteoh@google.com>
2026-06-18 09:54:50 -0700

    chore: use cobra flag group for config flags

--- cmd/root.go
+++ cmd/root.go
@@ -116,7 +116,7 @@ func NewCommand(opts *internal.ToolboxOptions) *cobra.Command {
 	// setup flags that are common across all commands
 	internal.PersistentFlags(cmd, opts)
 	flags := cmd.Flags()
-	internal.ConfigFileFlags(flags, opts)
+	internal.ConfigFileFlags(cmd, flags, opts)
 	internal.ServeFlags(flags, opts)
 	flags.BoolVar(&opts.Cfg.DisableReload, "disable-reload", false, "Disables dynamic reloading of tools file.")
 	flags.BoolVar(&opts.Cfg.IgnoreUnknownTools, "ignore-unknown-tools", false, "Log warnings and skip unknown/unsupported tool types instead of failing to start.")
